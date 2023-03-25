0258213a262c7c6bd02bbc9311b713a5badd7f71
Josphat Mwangi <josphatwanjiruw@gmail.com>
2023-03-25 23:44:48 +0300

    do routing to the pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { BlogComponent } from './pages/blog/blog.component';
+import { CategoryComponent } from './components/category/category.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:HomeComponent},
+  {path: 'blogs/:title', component:BlogComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
